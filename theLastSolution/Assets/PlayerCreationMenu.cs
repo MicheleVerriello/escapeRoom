2fb7465a2a4f96a0e1e465b5eee84806d44ccd2d
Michele Verriello <micverriello@gmail.com>
2022-12-04 19:59:45 +0100

    menu done - part 8

---
+++ theLastSolution/Assets/PlayerCreationMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using System.IO;
+
+public class PlayerCreationMenu : MonoBehaviour
+{
+    public GameObject player;
+    public List<OutfitChanger> outfitChangers = new List<OutfitChanger>();
+
+    public void RandomizePlayer()
+    {
+        foreach (OutfitChanger changer in outfitChangers)
+        {
+            changer.Randomize();
+        }
+    }
+
+    public void Save()
+    {
+        PrefabUtility.SaveAsPrefabAsset(player, "Assets/Player.prefab");
+        SceneManager.LoadScene(0);
+    }
+}
