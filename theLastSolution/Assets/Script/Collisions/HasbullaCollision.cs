24993f1ae0df7a9c47d2ec22323276cd725a7cac
Michele Verriello <micverriello@gmail.com>
2022-12-07 23:36:55 +0100

    Working on objects collision and dialogs between characters

--- theLastSolution/Assets/Script/Collisions/HasbullaCollision.cs
+++ theLastSolution/Assets/Script/Collisions/HasbullaCollision.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ChestCollision : MonoBehaviour
+public class HasbullaCollision : MonoBehaviour
 {
-    public GameObject gameHandler;
+
+    public GameObject canvasDialog;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +21,11 @@ public class ChestCollision : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        canvasDialog.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        canvasDialog.SetActive(false);
     }
 }
