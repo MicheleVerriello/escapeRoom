6ba90b1b1afc6b98b76b53ee8f037dab20486ac8
Michele Verriello <micverriello@gmail.com>
2022-12-09 17:53:27 +0100

    added collision audio on some objects

--- theLastSolution/Assets/Script/Collisions/PlayerCollisionWithEnemies.cs
+++ theLastSolution/Assets/Script/Collisions/PlayerCollisionWithEnemies.cs
@@ -8,6 +8,9 @@ public class PlayerCollisionWithEnemies : MonoBehaviour
 {
     public GameObject canvasGameOver;
     public Slider healthSlider;
+    public AudioSource enemyCollisionAudio;
+    public AudioSource wallCollisionAudio;
+    public AudioSource potionCollisionAudio;
     private float _collisionValue = 0.2f;
 
     private const string SWMAN1H = "SwordMan1h";
@@ -20,6 +23,14 @@ public class PlayerCollisionWithEnemies : MonoBehaviour
     private const string SWMAN3V = "SwordMan3v";
     private const string SWMAN4V = "SwordMan4v";
     private const string HealthPotion = "HealthPotion";
+    private const string Chest1 = "Chest1";
+    private const string Chest2 = "Chest2";
+    private const string Chest3 = "Chest3";
+    private const string Chest4 = "Chest4";
+    private const string Key1 = "Key1";
+    private const string Key2 = "Key2";
+    private const string Key3 = "Key3";
+    private const string Key4 = "Key4";
 
     private void OnCollisionEnter2D(Collision2D col)
     {
@@ -54,10 +65,44 @@ public class PlayerCollisionWithEnemies : MonoBehaviour
                 CollisionWithEnemy(col);
                 break;
             case HealthPotion:
+                potionCollisionAudio.Play();
                 healthSlider.value += _collisionValue * 2;
                 col.gameObject.SetActive(false);
                 break;
+            case Chest1:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Chest2:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Chest3:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Chest4:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Key1:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Key2:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Key3:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
+            case Key4:
+                Debug.Log("C1");
+                potionCollisionAudio.Play();
+                break;
             default:
+                wallCollisionAudio.Play();
                 Debug.Log("Collision with " + col.gameObject.name);
                 break;
         }
@@ -65,6 +110,7 @@ public class PlayerCollisionWithEnemies : MonoBehaviour
 
     private void CollisionWithEnemy(Collision2D col)
     {
+        enemyCollisionAudio.Play();
         col.gameObject.SetActive(false);
         if (healthSlider.value - _collisionValue <= 0)
         {
